3b9707114956074b140e3b26efdad037c793c63b
th30retical <tfjtan@hotmail.com>
2015-08-15 15:46:36 -0400

    scrolling image

--- www/js/app.js
+++ www/js/app.js
@@ -5,42 +5,48 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
+var game = new Game();
 angular.module('starter', ['ionic', 'controllers', 'services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    // if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-    //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    //   cordova.plugins.Keyboard.disableScroll(true);
-    //
-    // }
-    // if (window.StatusBar) {
-    //   // org.apache.cordova.statusbar required
-    //   StatusBar.styleLightContent();
-    // }
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
 
     // Matter.js module aliases
-    var Engine = Matter.Engine,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
+    // var Engine = Matter.Engine,
+    // World = Matter.World,
+    // Bodies = Matter.Bodies;
+    //
+    // // create a Matter.js engine
+    // var engine = Engine.create(document.body);
+    //
+    // // create two boxes and a ground
+    // var boxA = Bodies.rectangle(400, 200, 80, 80);
+    // var boxB = Bodies.rectangle(450, 50, 80, 80);
+    // var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    //
+    // // add all of the bodies to the world
+    // World.add(engine.world, [boxA, boxB, ground]);
+    //
+    // // run the engine
+    // Engine.run(engine);
+    //
+    // console.log("ran");
 
-    // create a Matter.js engine
-    var engine = Engine.create(document.body);
 
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    if (game.init())
+      game.start();
 
-    // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, ground]);
-
-    // run the engine
-    Engine.run(engine);
-
-    console.log("ran");
 
 
   });
@@ -64,3 +70,81 @@ angular.module('starter', ['ionic', 'controllers', 'services'])
   $urlRouterProvider.otherwise('/');
 
 });
+
+// this allows us to initalize pictures once and not multiple times
+var imageRepo = new function() {
+  this.empty = null;
+  this.background = new Image();
+
+  this.background.src = "img/bg.png";
+};
+
+function Drawable() {
+  this.init = function(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  this.speed = 0;
+  this.gravity = 1;
+  this.canvasWidth = 0;
+  this.canvasHeight = 0;
+
+  this.draw = function() {
+  }
+}
+
+function Background() {
+  this.speed = 1;
+
+  this.draw = function() {
+    this.x -= this.speed;
+
+    this.context.drawImage(imageRepo.background, this.x, this.y);
+    this.context.drawImage(imageRepo.background, this.x+this.canvasWidth, this.y);
+
+    if (this.x <= -this.canvasWidth) {
+      this.x = 0;
+    }
+  };
+}
+Background.prototype = new Drawable();
+
+function Game() {
+  this.init = function() {
+    this.bgCanvas = document.getElementById('background');
+    if (this.bgCanvas.getContext) {
+      this.bgContext = this.bgCanvas.getContext('2d');
+      console.log(this.bgContext);
+      Background.prototype.context = this.bgContext;
+			Background.prototype.canvasWidth = this.bgCanvas.width;
+			Background.prototype.canvasHeight = this.bgCanvas.height;
+
+      this.background = new Background();
+      this.background.init(0,0);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.start = function() {
+    animate();
+  };
+}
+
+function animate() {
+  requestAnimFrame( animate );
+  game.background.draw();
+}
+
+window.requestAnimFrame = (function(){
+  return window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
--- www/js/matter-test.js
+++ www/js/matter-test.js
@@ -15,7 +15,61 @@ function Drawable() {
   this.gravity = 1;
   this.canvasWidth = 0;
   this.canvasHeight = 0;
-  
+
   this.draw = function() {
   }
 }
+
+function Background() {
+  this.speed = 1;
+
+  this.draw = function() {
+    this.y += speed;
+    this.context.drawImage(imageRepo.background, this.x, this.y);
+    this.context.drawImage(imageRepo.background, this.x, this.y-this.canvasHeight);
+
+    if (this.y >= this.canvasHeight) {
+      this.y = 0;
+    }
+  };
+}
+Background.prototype = new Drawable();
+
+function Game() {
+  this.init = function() {
+    this.bgCanvas = document.getElementById('background');
+
+    if (this.bgCanvas.getContext) {
+      this.bgCanvas = this.bgCanvas.getContext('2d');
+
+      Background.prototype.context = this.bgContext;
+			Background.prototype.canvasWidth = this.bgCanvas.width;
+			Background.prototype.canvasHeight = this.bgCanvas.height;
+
+      this.background = new Background();
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.start = function() {
+    animate();
+  };
+}
+
+function animate() {
+  requestAnimFrame( animate );
+  game.background.draw();
+}
+
+window.requestAnimFrame = (function(){
+  return window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
